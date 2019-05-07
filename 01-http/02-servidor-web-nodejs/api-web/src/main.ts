cf0b2d46c957a31923ed3251e8d92f0e590d6af4
Gorky Andrés Muñoz <gorkymunoz@hotmail.com>
2019-05-07 15:57:44 -0500

    html

--- 01-http/02-servidor-web-nodejs/api-web/src/main.ts
+++ 01-http/02-servidor-web-nodejs/api-web/src/main.ts
@@ -2,17 +2,25 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {NestExpressApplication} from "@nestjs/platform-express";
 import {join} from "path";
+import * as express from 'express';
+import * as path from "path";
+import * as favicon from "serve-favicon"
+
+
 
 const cookieParser = require('cookie-parser');
+
 
 async function bootstrap() {
   const app = await NestFactory
       .create(AppModule) as NestExpressApplication; //Casteo
+  app.use(favicon(path.join(__dirname,'..','publico','imagenes','planetexpress.ico')));
 
   app.use(cookieParser('secreto'));
   //Implementación EJS
   app.setViewEngine('ejs');
   app.setBaseViewsDir(join(__dirname,'..','views'));
+  app.use(express.static('publico'));
   await app.listen(3000);
 }
 bootstrap();
