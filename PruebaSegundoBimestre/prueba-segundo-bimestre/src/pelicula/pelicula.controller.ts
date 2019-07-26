666b3ef9448bfb80231a2f960b3c2bf7d083a3fd
Gorky Muñoz <gorkymunoz@hotmail.com>
2019-07-26 11:58:46 -0600

    nueva película

--- PruebaSegundoBimestre/prueba-segundo-bimestre/src/pelicula/pelicula.controller.ts
+++ PruebaSegundoBimestre/prueba-segundo-bimestre/src/pelicula/pelicula.controller.ts
@@ -20,11 +20,11 @@ export class PeliculaController {
         const idActor = body.idActor;
         const actor = await this._ActorService.consultarActorPorId(idActor);
         const nombreActor = actor.nombresActor + " " + actor.apellidosActor;
-        res.render('admin/nueva-pelicula',{nombreActor})
+        res.render('admin/nueva-pelicula',{nombreActor,idActor})
     }
 
-    @Post('listar-peliculas/:idActor')
-    async postListarPeliculas(@Param() param,@Res() res){
+    @Get('listar-peliculas/:idActor')
+    async getListarPeliculas(@Param() param,@Res() res){
         const idActor = Number(param.idActor);
         const peliculas = await this._PeliculaService.consultarPeliculasPorActor(idActor);
         res.render('admin/listar-peliculas',{peliculas,idActor});
