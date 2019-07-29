e412b664f5ffd136b3be3790fcf39c8b57310b72
jsofii <sofi-0106@hotmail.com>
2019-07-29 01:48:48 -0500

    falta búsqueda de combo

--- proyectop/proyectop/src/ProductoPromocion/producto.promocion.controller.ts
+++ proyectop/proyectop/src/ProductoPromocion/producto.promocion.controller.ts
@@ -1,8 +1,10 @@
-import {Controller, Get, Res} from "@nestjs/common";
+import {Body, Controller, Get, Post, Res} from "@nestjs/common";
 import {ProductoPromocionService} from './producto.promocion.service';
 import {AppService} from '../app.service';
 import {ProductoEntity} from '../Producto/producto.entity';
 import {Param} from '@nestjs/common/decorators/http/route-params.decorator';
+import {getConnection} from 'typeorm';
+import {ProductoPromocionEntity} from './producto.promocion.entity';
 
 @Controller('api/producto-promocion')
 export class ProductoPromocionController {
@@ -45,6 +47,29 @@ export class ProductoPromocionController {
         const nombre= this.nombreCombo
         // this._combosService.platosDisponibles=arre
         return res.render('productosPromocion', {arregloDisponibles, arregloSeleccionados,nombre })
+    }
+    @Post('addProducto')
+    addElementeo(@Res() res, @Body('index') indexBody: number) {
+        console.log(indexBody)
+        const plato: ProductoEntity = this._appService.productosDisponibles[indexBody]
+        const x = this._productoPromocionService.crearRelacion(plato.id, this.idCombo).then(
+            value1 => {
+                console.log('creado')
+                res.redirect('/api/producto-promocion/ver/'+this.idCombo)
+            }
+        )
+
+
+
+
+    }
+    @Post('rmvPlato')
+    async rmvElementeo(@Res() res, @Body('index') indexBody) {
+        console.log(indexBody)
+        const plato: ProductoEntity = this._appService.productosSeleccionado[indexBody]
+        await getConnection().createQueryBuilder().delete().from(ProductoPromocionEntity).where("productoId = :productoId", {promocionId: this.idCombo}).where("productoId = :productoId", {productoId: plato.id}).execute()
+
+        res.redirect('/api/producto-promocion/ver/'+this.idCombo)
     }
 
 
--- proyectop/proyectop/src/ProductoPromocion/producto.promocion.service.ts
+++ proyectop/proyectop/src/ProductoPromocion/producto.promocion.service.ts
@@ -3,23 +3,44 @@ import {ProductoEntity} from '../Producto/producto.entity';
 import {Repository} from 'typeorm';
 import {InjectRepository} from "@nestjs/typeorm";
 import {AppService} from '../app.service';
+import {ProductoPromocionEntity} from './producto.promocion.entity';
 
 @Injectable()
 export class ProductoPromocionService {
     query:string
     constructor(@InjectRepository(ProductoEntity)
-                private readonly _productosRepository: Repository<ProductoEntity>,private readonly _appService: AppService){
+                private readonly _productosRepository: Repository<ProductoEntity>,private readonly _appService: AppService
+    ,@InjectRepository(ProductoPromocionEntity)
+                private readonly _productoPromocionRepository: Repository<ProductoPromocionEntity>){
 
     }
     buscar(){
         return this._productosRepository.find()
+    }
+    crearRelacion(idProducto: number, idPromocion: number) {
+        const relacion = {
+
+            producto: idProducto,
+            promocion: idPromocion
+        };
+
+
+        const objeto = this._productoPromocionRepository.create(relacion)
+        return this._productoPromocionRepository.save(objeto).then(
+            value => {
+                console.log('todo bien', value)
+            },
+            onerror => {
+                console.log('error', onerror)
+            }
+        )
     }
     platosSeleccionados(idCombo:string){
         console.log("select productoId as id ,nombreProducto nombre, precioProducto as precio from bdproductopromocion, dbproducto where bdproductopromocion.productoId=dbproducto.id and bdproductopromocion.promocionId="+idCombo)
         this.query ="select productoId as id ,nombreProducto nombre, precioProducto as precio from bdproductopromocion, dbproducto where bdproductopromocion.productoId=dbproducto.id and bdproductopromocion.promocionId="+idCombo
         this._productosRepository.query(this.query).then(
             value => {
-                console.log('RESULTADO DE MI QUERY', value)
+                // console.log('RESULTADO DE MI QUERY', value)
             },
             error=>{
                 console.log(error)
